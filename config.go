78b4e759efbe0933089739b771aa7b26e6c832f5
n4tsumi <67434791+n4tsumi@users.noreply.github.com>
2020-07-12 15:46:37 +0900

    major change

---
+++ config.go
@@ -0,0 +1,13 @@
+package lineGo
+
+const (
+	LineHost = "https://legy-jp-addr.line.naver.jp"
+	Register = "/api/v4/TalkService.do"
+	Normal   = LineHost + "/S4"
+	Polling  = LineHost + "/P4"
+
+	SystemName = "lineGo"
+
+	UserAgent = "Line/5.24.1"
+	LineApp   = "DESKTOPMAC\t5.24.1\tOS X\t10.15.1"
+)
--- line/poll.go
+++
@@ -1,9 +0,0 @@
-package line
-
-import (
-	service "../talkservice"
-)
-
-func (self Client) FetchOperations() ([]*service.Operation, error) {
-	return self.poll.FetchOperations(self.ctx, self.Revision, 50)
-}
